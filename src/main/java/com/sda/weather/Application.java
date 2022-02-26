d0e3620e8ff5fb8501baf2200edad7c636aa7d7c
kuczumulunga <kuczumulunga123@wp.pl>
2022-02-26 11:52:25 +0100

    second commit

--- src/main/java/com/sda/weather/Application.java
+++ src/main/java/com/sda/weather/Application.java
@@ -2,7 +2,7 @@ package com.sda.weather;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
         System.out.println("Michal tu byl");
     }
 }
