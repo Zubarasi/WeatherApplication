59d7fe34d2076c9a17a12c16570713a6081caff4
Michal Paukszto <michal.paukszto@gmail.com>
2022-02-26 11:25:00 +0100

    message

--- src/main/java/com/sda/weather/Application.java
+++ src/main/java/com/sda/weather/Application.java
@@ -3,5 +3,6 @@ package com.sda.weather;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Michal tu byl");
     }
 }
