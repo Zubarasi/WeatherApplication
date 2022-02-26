4e727d517bfa07eb5844e384614db9bcb4d08306
kuczumulunga <kuczumulunga123@wp.pl>
2022-02-26 11:38:49 +0100

    2 commit

---
+++ src/main/java/com/sda/weather/UserInterface.java
@@ -0,0 +1,30 @@
+package com.sda.weather;
+
+import java.util.Scanner;
+
+public class UserInterface {
+
+    public void run() {
+        System.out.println("Aplikacja jest uruchomiona\n");
+
+        Scanner scanner = new Scanner(System.in);
+    while (true) {
+        System.out.println("Witaj w elektronicznym dzienniku, co chcesz zrobić?");
+        System.out.println("1. Dodać nowy wpis");
+        System.out.println("2. Odczytać wszystkie wpisy");
+        System.out.println("0. Zamknąć aplikację");
+
+        int option = scanner.nextInt();
+
+        switch (option) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 0:
+                return;
+        }
+    }
+}
+
+}
